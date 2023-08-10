242da17f8a5ba8002af670290a7b9f920f6a95ef
Anh Phan <anhphan@Anhs-MacBook-Pro.local>
2023-08-10 21:05:57 +0700

    Show Counter feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Intro to Redux</h1>
-      {/* <Counter /> */}
+      <Counter />
       <MultiCounter />
       <Todo />
     </div>
